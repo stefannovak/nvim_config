4214622765fa4ff8771c2f93a40bbfd2bdcb3f9d
Stefan Novak - Laptop <stefannovak96@gmail.com>
2025-07-12 14:39:10 +0100

    feat: which key

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -31,4 +31,17 @@ require("lazy").setup({
     end,
   },
   {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
 })
